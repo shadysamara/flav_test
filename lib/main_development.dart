e54aac55c87a864bab6273e096a7f73d881809d1
shady <shady.samara@gmail.com>
2024-07-13 21:13:52 +0300

    1- initialize launch.json file 2- create development main 3- create production main

---
+++ lib/main_development.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    systemNavigationBarColor: Colors.red, // navigation bar color
+    statusBarColor: Colors.red, // status bar color
+  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return const MaterialApp(
+      debugShowCheckedModeBanner: true,
+      home: Scaffold(
+        body: Text("This is development mode"),
+      ),
+    );
+  }
+}
